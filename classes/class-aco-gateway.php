66a76e68f9600eeaa6a139be22a851e56a313fdd
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-03-13 16:21:09 +0100

    Add process payment handler

--- classes/class-aco-gateway.php
+++ classes/class-aco-gateway.php
@@ -93,6 +93,54 @@ class ACO_Gateway extends WC_Payment_Gateway {
 	}
 
 	/**
+	 * Process the payment with information from Avarda and return the result.
+	 *
+	 * @param  int $order_id WooCommerce order ID.
+	 *
+	 * @return mixed
+	 */
+	public function process_payment_handler( $order_id ) {
+		// Get the Avarda order ID.
+		$order = wc_get_order( $order_id );
+		if ( is_object( $order ) && $order->get_transaction_id() ) {
+			$avarda_purchase_id = $order->get_transaction_id();
+		} else {
+			$avarda_purchase_id = WC()->session->get( 'aco_wc_purchase_id' );
+		}
+
+		$avarda_order = ACO_WC()->api->request_get_payment( $avarda_purchase_id );
+		if ( ! $avarda_order ) {
+			return false;
+		}
+
+		if ( $order_id && $avarda_order ) {
+
+			// Set WC order transaction ID.
+			update_post_meta( $order_id, '_wc_avarda_purchase_id', sanitize_key( $avarda_order['purchaseId'] ) );
+
+			update_post_meta( $order_id, '_transaction_id', sanitize_key( $avarda_order['purchaseId'] ) );
+
+			$environment = $this->testmode ? 'test' : 'live';
+			update_post_meta( $order_id, '_wc_avarda_environment', $environment );
+
+			$avarda_country = wc_get_base_location()['country'];
+			update_post_meta( $order_id, '_wc_avarda_country', $avarda_country );
+
+			$order->save();
+			// Let other plugins hook into this sequence.
+			do_action( 'aco_wc_process_payment', $order_id, $avarda_order );
+
+			// Check that the transaction id got set correctly.
+			if ( strtolower( get_post_meta( $order_id, '_transaction_id', true ) ) === strtolower( $avarda_purchase_id ) ) {
+				return true;
+			}
+		}
+		// Return false if we get here. Something went wrong.
+		return false;
+	}
+
+
+	/**
 	 * Initialise Gateway Settings Form Fields.
 	 *
 	 * @return void
