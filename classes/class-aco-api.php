e056aefcec356c4eafe9280f8a690e512052e4fa
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-03-18 15:42:47 +0100

    Add easy access to activate order function

--- classes/class-aco-api.php
+++ classes/class-aco-api.php
@@ -77,6 +77,18 @@ class ACO_API {
 	}
 
 	/**
+	 * Activates an Avarda Checkout order.
+	 *
+	 * @param int $order_id WC order id.
+	 * @return mixed
+	 */
+	public function request_activate_order( $order_id ) {
+		$request  = new ACO_Request_Activate_Order();
+		$response = $request->request( $order_id );
+		return $this->check_for_api_error( $response );
+	}
+
+	/**
 	 * Checks for WP Errors and returns either the response as array or a false.
 	 *
 	 * @param array $response The response from the request.
