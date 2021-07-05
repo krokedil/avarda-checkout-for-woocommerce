b2bb8ef0b0d124b6ab9e8996d3be94ac69191a00
Niklas Högefjord <niklas@krokedil.se>
2021-07-05 10:37:49 +0200

    Adds support for sending in termsAndConditionsUrl

--- classes/requests/helpers/class-aco-helper-checkout-setup.php
+++ classes/requests/helpers/class-aco-helper-checkout-setup.php
@@ -21,10 +21,16 @@ class ACO_Helper_Checkout_Setup {
 	 */
 	public function get_checkout_setup( $cart = null ) {
 		$checkout_setup = array();
+		$terms_url      = $this->get_terms_url();
 
 		$checkout_setup['mode']                     = 'B2C'; // TODO: Logic for using correct value depending on customer type.
 		$checkout_setup['completedNotificationUrl'] = get_home_url() . '/wc-api/ACO_WC_Notification';
 		$checkout_setup['language']                 = $this->get_language();
+
+		if ( ! empty( $terms_url ) ) {
+			$checkout_setup['termsAndConditionsUrl'] = $terms_url;
+		}
+
 		return $checkout_setup;
 	}
 
@@ -55,6 +61,19 @@ class ACO_Helper_Checkout_Setup {
 				break;
 		}
 		return $language;
+	}
+
+	/**
+	 * Terms URL.
+	 *
+	 * Required. URL of merchant terms and conditions.
+	 *
+	 * @return string
+	 */
+	private function get_terms_url() {
+		$terms_url = get_permalink( wc_get_page_id( 'terms' ) );
+
+		return apply_filters( 'aco_wc_terms_url', $terms_url );
 	}
 
 }
