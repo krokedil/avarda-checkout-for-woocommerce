10a60880264dd5b68610fd67f3fa7bd50da44236
Martin Lindahl <84283336+krokedilmartin@users.noreply.github.com>
2023-11-01 20:53:53 +0100

    working wip

--- classes/class-aco-order-management.php
+++ classes/class-aco-order-management.php
@@ -146,7 +146,8 @@ class ACO_Order_Management {
 			$order->set_status( 'on-hold' );
 		} else {
 			// Add time stamp, used to prevent duplicate activations for the same order.
-			update_post_meta( $order_id, '_avarda_reservation_activated', current_time( 'mysql' ) );
+			$order->update_meta_data( $order_id, '_avarda_reservation_activated', current_time( 'mysql' ) );
+			$order->save();
 			$order->add_order_note( __( 'Avarda reservation was successfully activated.', 'avarda-checkout-for-woocommerce' ) );
 		}
 
@@ -320,7 +321,8 @@ class ACO_Order_Management {
 				return;
 			}
 			$order->add_order_note( __( 'Avarda Checkout order was successfully updated.', 'avarda-checkout-for-woocommerce' ) );
-			update_post_meta( $order_id, '_avarda_payment_amount', $order->get_total() );
+			$order->update_meta_data( $order_id, '_avarda_payment_amount', $order->get_total() );
+			$order->save();
 			return;
 		}
 	}
