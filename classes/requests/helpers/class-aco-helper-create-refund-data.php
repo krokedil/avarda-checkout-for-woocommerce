d252ef60adcd9c222006e77ec9c8cc200622ca55
Montazar Al-Jaber <github.v0its@slmail.me>
2023-11-09 16:47:42 +0100

    Retrieve refund data directly from WC_Order_Refund

--- classes/requests/helpers/class-aco-helper-create-refund-data.php
+++ classes/requests/helpers/class-aco-helper-create-refund-data.php
@@ -93,24 +93,11 @@ class ACO_Helper_Create_Refund_Data {
 	 * @return string
 	 */
 	public static function get_refunded_order( $order_id ) {
-		$query_args      = array(
-			'fields'         => 'id=>parent',
-			'post_type'      => 'shop_order_refund',
-			'post_status'    => 'any',
-			'posts_per_page' => -1,
-		);
-		//Montazar fixar: https://app.clickup.com/t/86931kna5
-		$refunds         = get_posts( $query_args );
-		$refund_order_id = array_search( $order_id, $refunds );
-		if ( is_array( $refund_order_id ) ) {
-			foreach ( $refund_order_id as $key => $value ) {
-				if ( ! get_post_meta( $value, '_krokedil_refunded' ) ) {
-					$refund_order_id = $value;
-					break;
-				}
-			}
-		}
-		return $refund_order_id;
+		$order   = wc_get_order( $order_id );
+		$refunds = $order->get_refunds();
+		$refund  = reset( $refunds );
+
+		return $refund->get_id();
 	}
 
 	/**
