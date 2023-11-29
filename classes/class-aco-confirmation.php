0a232c701796f0af419ceae329d05e89eaa01b39
MichaelBengtsson <michael@krokedil.se>
2023-11-29 11:54:23 +0100

    Fix bad meta query and improve verification of order before processing in confirmation

--- classes/class-aco-confirmation.php
+++ classes/class-aco-confirmation.php
@@ -50,36 +50,28 @@ class ACO_Confirmation {
 		$order_key          = filter_input( INPUT_GET, 'key', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
 		$order_id           = filter_input( INPUT_GET, 'wc_order_id', FILTER_SANITIZE_NUMBER_INT );
 
-		// Return if we dont have our parameters set.
+		// Return if we don't have our parameters set.
 		if ( empty( $aco_confirm ) || empty( $avarda_purchase_id ) ) {
 			return;
 		}
 
 		$order = wc_get_order( $order_id );
-		$order = empty( $order ) || $order->get_meta( '_wc_avarda_purchase_id' ) !== $avarda_purchase_id ? false : $order;
+		$order = empty( $order ) ? false : $order;
+
 		if ( empty( $order ) ) {
 			// Find relevant order in Woo.
-			$orders = wc_get_orders(
-				array(
-					'meta_query' => array(
-						'meta_key'   => '_wc_avarda_purchase_id',
-						'meta_value' => $avarda_purchase_id,
-						'limit'      => 1,
-						'orderby'    => 'date',
-						'order'      => 'DESC',
-						'compare'    => '=',
-					),
-				)
-			);
-
-			$order = reset( $orders );
-
+			$order = aco_get_order_by_purchase_id( $avarda_purchase_id );
 		}
 
 		if ( empty( $order ) ) {
 			$order_key = sanitize_text_field( $order_key );
 			$order_id  = wc_get_order_id_by_order_key( $order_key );
 			$order     = wc_get_order( $order_id );
+		}
+
+		// Verify the meta data after we get an order, to ensure that we don't process a wrong order.
+		if ( $order->get_meta( '_wc_avarda_purchase_id' ) !== $avarda_purchase_id ) {
+			$order = false;
 		}
 
 		if ( ! $order ) {
