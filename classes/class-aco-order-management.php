29e61c880838b6febf2538708c71ce9c715ebd1e
MichaelBengtsson <michael@krokedil.se>
2024-02-21 11:32:30 +0100

    Disable changing of pickup point in the order metabox

--- classes/class-aco-order-management.php
+++ classes/class-aco-order-management.php
@@ -34,7 +34,9 @@ class ACO_Order_Management {
 
 		// Add the shipping metabox to the edit order page.
 		$shipping_edit_order_page = new EditOrderPage( ACO_WC()->pickup_points );
-		$shipping_edit_order_page->set_metabox_title( __( 'Avarda Shipping Information', 'avarda-checkout-for-woocommerce' ) );
+		$shipping_edit_order_page
+			->set_metabox_title( __( 'Avarda Shipping Information', 'avarda-checkout-for-woocommerce' ) )
+			->set_can_change_pickup_point( false );
 	}
 
 	/**
