180cb7deb5eb5a8691acce7e3a631eb0c4c4e95b
MichaelBengtsson <michael@krokedil.se>
2024-08-29 10:25:26 +0200

    Update settings to combine avarda partner and woocommerce shipping integration to single select box

--- includes/aco-form-fields.php
+++ includes/aco-form-fields.php
@@ -9,34 +9,34 @@ $token_suggestion = ACO_API_Registry::get_token_suggestion();
 $token_suggestion = "<code>{$token_suggestion}</code>";
 
 $settings = array(
-	'enabled'                         => array(
+	'enabled'                      => array(
 		'title'   => __( 'Enable/Disable', 'avarda-checkout-for-woocommerce' ),
 		'type'    => 'checkbox',
 		'label'   => __( 'Enable ' . $this->method_title, 'avarda-checkout-for-woocommerce' ), // phpcs:ignore
 		'default' => 'yes',
 	),
-	'title'                           => array(
+	'title'                        => array(
 		'title'       => __( 'Title', 'avarda-checkout-for-woocommerce' ),
 		'type'        => 'text',
 		'description' => __( 'This controls the title which the user sees during checkout.', 'avarda-checkout-for-woocommerce' ),
 		'default'     => __( $this->method_title, 'avarda-checkout-for-woocommerce' ), // phpcs:ignore
 		'desc_tip'    => true,
 	),
-	'description'                     => array(
+	'description'                  => array(
 		'title'       => __( 'Description', 'avarda-checkout-for-woocommerce' ),
 		'type'        => 'textarea',
 		'default'     => __( 'Pay with Avarda via invoice, card and direct bank payments.', 'avarda-checkout-for-woocommerce' ),
 		'desc_tip'    => true,
 		'description' => __( 'This controls the description which the user sees during checkout.', 'avarda-checkout-for-woocommerce' ),
 	),
-	'select_another_method_text'      => array(
+	'select_another_method_text'   => array(
 		'title'       => __( 'Other payment method button text', 'avarda-checkout-for-woocommerce' ),
 		'type'        => 'text',
 		'description' => __( 'Customize the <em>Select another payment method</em> button text that is displayed in checkout if using other payment methods than Avarda Checkout. Leave blank to use the default (and translatable) text.', 'avarda-checkout-for-woocommerce' ),
 		'default'     => '',
 		'desc_tip'    => true,
 	),
-	'checkout_flow'                   => array(
+	'checkout_flow'                => array(
 		'title'       => __( 'Checkout flow', 'avarda-checkout-for-woocommerce' ),
 		'type'        => 'select',
 		'options'     => array(
@@ -47,71 +47,69 @@ $settings = array(
 		'default'     => 'embedded',
 		'desc_tip'    => false,
 	),
-	'testmode'                        => array(
+	'testmode'                     => array(
 		'title'   => __( 'Testmode', 'avarda-checkout-for-woocommerce' ),
 		'type'    => 'checkbox',
 		'label'   => __( 'Enable Avarda Checkout testmode', 'avarda-checkout-for-woocommerce' ),
 		'default' => 'no',
 	),
-	'age_validation'                  => array(
+	'age_validation'               => array(
 		'title'       => __( 'Age validation', 'avarda-checkout-for-woocommerce' ),
 		'type'        => 'number',
 		'description' => __( 'Enter an age if you only want to offer purchases from customers older than the specified age. Leave blank or set to 0 to disable age validation.', 'avarda-checkout-for-woocommerce' ),
 		'default'     => '',
 		'desc_tip'    => false,
 	),
-	'order_management'                => array(
+	'order_management'             => array(
 		'title'   => __( 'Enable Order Management', 'avarda-checkout-for-woocommerce' ),
 		'type'    => 'checkbox',
 		'label'   => __( 'Enable Avarda order capture on WooCommerce order completion and Avarda order cancellation on WooCommerce order cancellation', 'avarda-checkout-for-woocommerce' ),
 		'default' => 'yes',
 	),
-	'debug'                           => array(
+	'debug'                        => array(
 		'title'       => __( 'Debug Log', 'avarda-checkout-for-woocommerce' ),
 		'type'        => 'checkbox',
 		'label'       => __( 'Enable logging', 'avarda-checkout-for-woocommerce' ),
 		'default'     => 'no',
 		'description' => __( 'Log ' . $this->method_title . ' events in the WooCommerce status logs', 'avarda-checkout-for-woocommerce' ), // phpcs:ignore
 	),
-	'payment_gateway_icon'            => array(
+	'payment_gateway_icon'         => array(
 		'title'       => __( 'Payment gateway icon', 'avarda-checkout-for-woocommerce' ),
 		'type'        => 'text',
 		'description' => __( 'Enter an URL to the icon you want to display for the payment method. Use <i>default</i> to display the Avarda logo. Leave blank to not show an icon at all.', 'avarda-checkout-for-woocommerce' ),
 		'default'     => 'default',
 		'desc_tip'    => false,
 	),
-	'payment_gateway_icon_width'      => array(
+	'payment_gateway_icon_width'   => array(
 		'title'       => __( 'Payment gateway icon width', 'avarda-checkout-for-woocommerce' ),
 		'type'        => 'number',
 		'description' => __( 'Specify the max width (in px) of the payment gateway icon.', 'avarda-checkout-for-woocommerce' ),
 		'default'     => '',
 		'desc_tip'    => true,
 	),
-	'two_column_checkout'             => array(
+	'two_column_checkout'          => array(
 		'title'   => __( 'Two column checkout layout', 'avarda-checkout-for-woocommerce' ),
 		'type'    => 'checkbox',
 		'label'   => __( 'Enable Two column checkout layout', 'avarda-checkout-for-woocommerce' ),
 		'default' => 'yes',
 	),
 	// Integrated shipping settings.
-	'integrated_shipping_settings'    => array(
+	'integrated_shipping_settings' => array(
 		'title' => __( 'Shipping', 'avarda-checkout-for-woocommerce' ),
 		'type'  => 'title',
 	),
-	'integrated_shipping'             => array(
-		'title'   => __( 'Integrated Shipping', 'avarda-checkout-for-woocommerce' ),
-		'type'    => 'checkbox',
-		'label'   => __( 'Enable Avarda Checkout integrated shipping with for example Ingrid or nShift.', 'avarda-checkout-for-woocommerce' ),
-		'default' => 'no',
+	'integrated_shipping'          => array(
+		'title'       => __( 'Integrated Shipping', 'avarda-checkout-for-woocommerce' ),
+		'type'        => 'select',
+		'description' => __( 'Select which type of shipping you want to integrate in Avarda Checkout. This will display the shipping methods for the customer inside the Avarda Checkout instead of the WooCommerce order summary.', 'avarda-checkout-for-woocommerce' ),
+		'options'     => array(
+			''            => __( 'None', 'avarda-checkout-for-woocommerce' ),
+			'woocommerce' => __( 'WooCommerce Shipping', 'avarda-checkout-for-woocommerce' ),
+			'avarda'      => __( 'Partner Shipping', 'avarda-checkout-for-woocommerce' ),
+		),
+		'default'     => '',
 	),
-	'integrated_shipping_woocommerce' => array(
-		'title'       => __( 'Integrated WooCommerce Shipping', 'avarda-checkout-for-woocommerce' ),
-		'type'        => 'checkbox',
-		'label'       => __( 'Enable if you want to use the WooCommerce shipping methods inside the Avarda checkout.', 'avarda-checkout-for-woocommerce' ),
-		'description' => __( 'You need to configure this with Avarda.', 'avarda-checkout-for-woocommerce' ),
-		'default'     => 'no',
-	),
-	'shipping_broker_api_key'         => array(
+	'shipping_broker_api_key'      => array(
 		'title'             => __( 'Shipping Broker API Key', 'avarda-checkout-for-woocommerce' ),
 		'type'              => 'password',
 		// translators: %s: token suggestion.
@@ -123,85 +121,85 @@ $settings = array(
 		),
 	),
 	// SE.
-	'credentials_se'                  => array(
+	'credentials_se'               => array(
 		'title' => 'API Credentials Sweden',
 		'type'  => 'title',
 	),
-	'merchant_id_se'                  => array(
+	'merchant_id_se'               => array(
 		'title'    => __( 'Client ID', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
 		'desc_tip' => true,
 	),
-	'api_key_se'                      => array(
+	'api_key_se'                   => array(
 		'title'    => __( 'Client Secret', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
 		'desc_tip' => true,
 	),
 	// NO.
-	'credentials_no'                  => array(
+	'credentials_no'               => array(
 		'title' => 'API Credentials Norway',
 		'type'  => 'title',
 	),
-	'merchant_id_no'                  => array(
+	'merchant_id_no'               => array(
 		'title'    => __( 'Client ID', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
 		'desc_tip' => true,
 	),
-	'api_key_no'                      => array(
+	'api_key_no'                   => array(
 		'title'    => __( 'Client Secret', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
 		'desc_tip' => true,
 	),
 	// DK.
-	'credentials_dk'                  => array(
+	'credentials_dk'               => array(
 		'title' => 'API Credentials Denmark',
 		'type'  => 'title',
 	),
-	'merchant_id_dk'                  => array(
+	'merchant_id_dk'               => array(
 		'title'    => __( 'Client ID', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
 		'desc_tip' => true,
 	),
-	'api_key_dk'                      => array(
+	'api_key_dk'                   => array(
 		'title'    => __( 'Client Secret', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
 		'desc_tip' => true,
 	),
 	// FI.
-	'credentials_fi'                  => array(
+	'credentials_fi'               => array(
 		'title' => 'API Credentials Finland',
 		'type'  => 'title',
 	),
-	'merchant_id_fi'                  => array(
+	'merchant_id_fi'               => array(
 		'title'    => __( 'Client ID', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
 		'desc_tip' => true,
 	),
-	'api_key_fi'                      => array(
+	'api_key_fi'                   => array(
 		'title'    => __( 'Client Secret', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
 		'desc_tip' => true,
 	),
 	// International.
-	'credentials_international'       => array(
+	'credentials_international'    => array(
 		'title' => 'API Credentials International',
 		'type'  => 'title',
 	),
-	'merchant_id_int'                 => array(
+	'merchant_id_int'              => array(
 		'title'    => __( 'Client ID', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
 		'desc_tip' => true,
 	),
-	'api_key_int'                     => array(
+	'api_key_int'                  => array(
 		'title'    => __( 'Client Secret', 'avarda-checkout-for-woocommerce' ),
 		'type'     => 'text',
 		'default'  => '',
