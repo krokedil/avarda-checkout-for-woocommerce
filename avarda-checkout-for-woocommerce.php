c15f15eab0c3f177759cf7e104a9a3630a668d8d
anyakro <anya@krokedil.se>
2026-05-04 10:54:59 +0200

    Version 1.17.6
    * Enhancement - Updated the settings UI for improved usability.
    * Tweak - Logging is now enabled by default.
    * Fix - Ensured payment-processing error messages are displayed correctly after recent WooCommerce changes.

--- avarda-checkout-for-woocommerce.php
+++ avarda-checkout-for-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Avarda Checkout for WooCommerce
  * Plugin URI:      http://krokedil.com/
  * Description:     Provides an Avarda Checkout gateway for WooCommerce.
- * Version:         1.17.5
+ * Version:         1.17.6
  * Author:          Krokedil
  * Author URI:      http://krokedil.com/
  * Developer:       Krokedil
@@ -12,9 +12,9 @@
  * Domain Path:     /languages
  *
  * WC requires at least: 5.6.0
- * WC tested up to: 10.4.3
+ * WC tested up to: 10.7.0
  *
- * Copyright:       © 2020-2025 Krokedil.
+ * Copyright:       © 2020-2026 Krokedil.
  * License:         GNU General Public License v3.0
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.html
  *
@@ -29,7 +29,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define plugin constants.
-define( 'AVARDA_CHECKOUT_VERSION', '1.17.5' );
+define( 'AVARDA_CHECKOUT_VERSION', '1.17.6' );
 define( 'AVARDA_CHECKOUT_URL', untrailingslashit( plugins_url( '/', __FILE__ ) ) );
 define( 'AVARDA_CHECKOUT_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'AVARDA_CHECKOUT_LIVE_ENV', 'https://checkout-api.avarda.com' );
